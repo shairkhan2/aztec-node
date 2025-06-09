248411b0ef7ed1725d39a3ae34755edfd09a225b
shair khan <130557140+shairkhan2@users.noreply.github.com>
2025-06-09 21:18:14 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -53,8 +53,9 @@ usermod -aG docker $SUDO_USER
 
 # Create and open screen session
 echo "Creating and attaching to Aztec screen session..."
-bash -c 'source ~/.bashrc && screen -S aztec -dm bash -c "aztec-up alpha-testnet && aztec-up 0.87.7 && echo -e \"\nAztec services are running. Press Ctrl+C to stop.\" && sleep infinity"'
+bash -c 'source ~/.bashrc && screen -S aztec -dm bash -c "aztec-up alpha-testnet && aztec-up 0.87.7 && echo -e \"\nAztec services are running. You can interact below. Use Ctrl+a d to detach.\" && exec bash"'
 screen -r aztec
+
 
 
 echo "----------------------------------------------------------"
